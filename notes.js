df160bdadfb386d5b09c1c67d4dea0cc68465d3b
ameerghazal <ameerghazal@uflip.com>
2023-09-29 22:08:21 -0500

    lol

---
+++ notes.js
@@ -0,0 +1,27 @@
+/*
+
+
+Section 3:
+
+What is react?
+- Components are the building blocks of use interfaces. 
+- Declarative syntax called JSX, which is an abstraction away from the DOM.
+- JSX: syntax that combines Js, HTML, CSS, etc. 
+- React reacts to state changes by rerendering the UI. It is techincally just a library. 
+
+
+Pure React
+- React can be rendered in other applications.
+- <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+  <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+- React is based on components. 
+- State is nesceassry to update the app on the screen.
+
+
+
+
+
+
+
+
+*/
